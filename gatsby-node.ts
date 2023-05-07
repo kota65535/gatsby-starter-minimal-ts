8699de45c26af89404bb9424e30b295225a95e6a
Tomohiko Ozawa <kota65535@gmail.com>
2023-05-07 15:05:08 +0900

    minimal reproduction

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,10 +1,9 @@
 import {GatsbyNode} from 'gatsby';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
-export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = async ({ actions }) => {
+// cf. https://www.gatsbyjs.com/docs/how-to/custom-configuration/add-custom-webpack-config/
+export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = async ({ actions, stage }) => {
   actions.setWebpackConfig({
-    plugins: [
-      new ForkTsCheckerWebpackPlugin()
-    ]
+    plugins: stage == 'develop-html' ? [] : [new ForkTsCheckerWebpackPlugin()]
   });
 };
